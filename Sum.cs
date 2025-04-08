09fef524eeecad1a179d2027c27c5939e60e8846
MuhammadAmmarulHaq11 <muhammadammarulhaq6@gmail.com>
2025-04-08 12:18:54 +0500

    Add files via upload

---
+++ Sum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_two_inegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 56;
+            int b = 65;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
