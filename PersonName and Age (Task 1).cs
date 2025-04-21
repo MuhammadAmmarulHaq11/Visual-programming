b8eed7b4a6f4966bc9e352c938a29dd6de351030
MuhammadAmmarulHaq11 <muhammadammarulhaq6@gmail.com>
2025-04-21 15:17:43 +0500

    Add files via upload

---
+++ PersonName and Age (Task 1).cs
@@ -0,0 +1,20 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonName_and_AGE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string personName;
+            personName = "Ammar-ul-haq";
+            int personAge = 21;
+            Console.WriteLine(personName);
+            Console.WriteLine(personAge);
+        }
+    }
+}
