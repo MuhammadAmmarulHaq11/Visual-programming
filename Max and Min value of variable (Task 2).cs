c49cca651577272e824b6d337583bffb8a9029a1
MuhammadAmmarulHaq11 <muhammadammarulhaq6@gmail.com>
2025-04-22 09:38:03 +0500

    Add files via upload

---
+++ Max and Min value of variable (Task 2).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max_and_Min_value_of_Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Int Max Value = " + int.MaxValue);
+            Console.WriteLine("Int Min Value = " + int.MinValue);
+            Console.WriteLine("Short Max Value = " + short.MaxValue);
+            Console.WriteLine("Short Min Value = " + short.MinValue);
+            Console.WriteLine("Long Max Value = " + long.MaxValue);
+            Console.WriteLine("Long Min Value = " + long.MinValue);
+            Console.WriteLine("Double Max Value = " + double.MaxValue);
+            Console.WriteLine("Double Min Value = " + double.MinValue);
+            Console.WriteLine("Float Max Value = " + float.MaxValue);
+            Console.WriteLine("Float Min Value = " + float.MinValue);
+            Console.WriteLine("Dec Max Value = " + decimal.MaxValue);
+            Console.WriteLine("Dec Min Value = " + decimal.MinValue);
+        }
+    }
+}
