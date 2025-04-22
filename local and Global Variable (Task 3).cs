e69b15b0b81f406a69675ae6fb9ad27bf0b9efc3
MuhammadAmmarulHaq11 <muhammadammarulhaq6@gmail.com>
2025-04-22 09:42:00 +0500

    Add files via upload

---
+++ local and Global Variable (Task 3).cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Local_and_Global_Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void age()
+            {
+                int personAge;//Local Variable
+                personAge = 21;
+                Console.WriteLine(personAge);
+            }
+
+            string personName; //Global Variable
+            personName = "Ammar-ul-haq";
+            Console.WriteLine(personName);
+            age();
+        }
+    }
+}
